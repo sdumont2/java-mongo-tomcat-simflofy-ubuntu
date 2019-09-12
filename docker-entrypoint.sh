9a3a7838deba030b19ee5c31dc3ab8744f531f22
Sean Dumont <sdumont92@gmail.com>
2019-09-12 15:59:10 -0400

    changing test role

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -315,7 +315,7 @@ if [ "$originalArgOne" = 'mongod' ]; then
 				db.createUser({
 					user: $(_js_escape "$MONGO_INITDB_ROOT_USERNAME"),
 					pwd: $(_js_escape "$MONGO_INITDB_ROOT_PASSWORD"),
-					roles: [ { role: 'root', db: $(_js_escape "${MONGO_INITDB_DATABASE:-test}") } ]
+					roles: [ { role: 'dbOwner', db: $(_js_escape "${MONGO_INITDB_DATABASE:-test}") } ]
 				})
 			EOJS
 		fi
